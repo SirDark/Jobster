6a23229e3a142f640b0613ce86708651422b6fd6
SirDark <gergelykevin123@gmail.com>
2023-12-24 15:11:07 +0100

    security

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,20 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/authentication')
 const testUser = require('../middleware/test-user')
+
+const rateLimiter = require('express-rate-limit')
+
+const apiLimiter = rateLimiter({
+    windowMs: 15*60*1000,
+    max: 100,
+    message:{
+        msg:'Too many requests from this IP, please try again after 15 minutes'
+    }
+})
+
 const { register, login, updateUser } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register',apiLimiter, register)
+router.post('/login',apiLimiter, login)
 router.patch('/updateUser', auth, testUser, updateUser)
 
 module.exports = router
